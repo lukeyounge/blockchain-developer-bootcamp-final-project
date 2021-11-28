f30338721affd9a22201a9ac2a11c91345b70c36
lukeyounge <80631053+lukeyounge@users.noreply.github.com>
2021-11-28 08:11:06 +0200

    updates to env

--- endpoint.js
+++
@@ -1,3 +0,0 @@
-const endpoint = 'https://api.nft.storage'; // the default
-
-const token = "API_KEY=\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEIwOUMyNjlFYTQ3NTlENThjNjkxRjg2N2NjMTI5MUEyNWU3RWIwM2IiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzAwNzMxMzk5MiwibmFtZSI6InRva2VubWludCJ9.O5dbXgHEk90doDUpxl5LDPGmoXs78CFavRGcvDMNzEM\" // your API key from https://nft.storage/manage";
